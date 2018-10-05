d88eb4718318f42bce20ba2be9790141fd9d595e
Peter Zlodej <peter.zlodej91@gmail.com>
2018-10-05 11:14:08 +0200

    Totalni error

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ class App extends Component {
             rel="noopener noreferrer"
           >
             Learn React Pero Franci
+            <padajn>
           </a>
         </header>
       </div>
