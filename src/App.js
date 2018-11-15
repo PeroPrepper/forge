52397c3d6e295e6088d11df9c94fe6bccb6d3406
Peter Zlodej <peter.zlodej91@gmail.com>
2018-11-15 20:39:53 +0100

    Added few buttons

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Button from "@material-ui/core/Button";
+import ButtonAppBar from "./components/ButtonAppBar";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <ButtonAppBar />
         <Button variant="contained" color="primary">
           Hello World
         </Button>
